8c2dfa05d265482c4d7b75f4bdd258518f35c133
vosid <temirboyev0823@gmail.com>
2022-09-14 13:55:47 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,79 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const loader = document.querySelector('.loader'),
+    age = document.querySelectorAll('.age')
+
+  // Loader
+  setTimeout(() => {
+    loader.style.opacity = '0'
+    setTimeout(() => {
+      loader.style.display = 'none'
+    }, 0)
+  }, 0)
+
+
+  // Countdown
+  let day = document.querySelector('.days')
+  let hour = document.querySelector('.hours')
+  let minute = document.querySelector('.minutes')
+  let second = document.querySelector('.seconds')
+  let mainBox = document.querySelector('.main_box')
+
+  function withO(an) {
+    if (an >= 0 && an < 10) {
+      return `0${an}`
+    } else {
+      return an
+    }
+  }
+
+  setInterval(function(){
+
+    let UpcomingDate = new Date('26 september 2022 00:00:00 AM');
+    let NowDate = new Date();
+    let differanceTime  = Math.floor((UpcomingDate-NowDate)/1000)
+
+    let differanceDay = Math.floor(differanceTime/60/60/24)
+    let differanceHour = Math.floor((differanceTime/60/60)%24)
+    let differanceMinute = Math.floor((differanceTime/60)%60)
+    let differanceSecound = Math.floor(differanceTime%60)
+
+    if(differanceDay >= 0){
+        mainBox.style.display = "block";
+        day.innerHTML =  differanceDay;
+        hour.innerHTML =  differanceHour;
+        minute.innerHTML = withO(differanceMinute);
+        second.innerHTML = withO(differanceSecound);
+    } else {
+      mainBox.style.display = "block";
+      day.innerHTML =  '0';
+      hour.innerHTML =  '0';
+      minute.innerHTML = '00';
+      second.innerHTML = '00';
+    }
+  },1000)
+
+
+  // progress bar
+  // scroller.addEventListener('scroll', (event) => {
+  //   p.style.width = pb(event.target) + '%'
+  // })
+
+  // function pb(progress) {
+  //   return progress.scrollTop / ((progress.scrollHeight - progress.clientHeight) / 100)
+  // }
+  
+  for (let i = 0; i < 101; i++) {
+    console.log(i);
+  }
+
+  // age
+  if (i == 100) {
+    age[0].classList.add('hide')
+    age[1].classList.add('ageSm')
+    age[2].classList.remove('active')
+    age[3].classList.add('active')
+    age[4].classList.remove('ageSm')
+    age[5].classList.remove('hide')
+  }
+
+})
